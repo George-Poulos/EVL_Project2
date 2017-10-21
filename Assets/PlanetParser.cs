15d94888a5078801bca1f307c94fe0c2a56e6c1e
ConditionalCoder <Georgepoulos5@gmail.com>
2017-10-21 14:37:14 -0500

    Parsing planet data

---
+++ Assets/PlanetParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PlanetParser
+{
+	public Dictionary<string, Dictionary<string, string[]>> dict = new Dictionary<string, Dictionary<string,string[]>>();
+
+	public PlanetParser (string fileName)
+	{
+		string text = System.IO.File.ReadAllText(fileName);
+		string[] csvLines = text.Split ('\n');
+		string[] keys;
+		int i = 0;
+		foreach (string line in csvLines) {
+			string[] items = line.Split (',');
+			if (i == 0) {
+				keys = items;
+			}
+			else{
+				Dictionary<string, string[]> tmp = new Dictionary<string,string[]> ();
+				tmp.Add (items [2], items);
+				if (dict.ContainsKey (items [1])) {
+					dict [items [1]].Add (items[2],items);
+				} else {
+					
+					dict.Add (items [1], tmp);
+				}
+			}
+			i++;
+		}
+	}
+}
+
