b4d20e0309d0e24a581c76a233d895affd1b3d16
ConditionalCoder <Georgepoulos5@gmail.com>
2017-10-21 23:50:46 -0500

    Optimized

---
+++ Assets/SolarSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class SolarSystem
+{
+	public Star star;
+	public List<Planet> planets = new List<Planet>(); 
+	public int numOfPlanets;
+
+	public SolarSystem (Star s)
+	{
+		this.star = s;
+		this.numOfPlanets = Int32.Parse(s.numberOfPlanets);
+	}
+
+	public void addPlanet(Planet p){
+		planets.Add (p);
+	}
+}
+
+
