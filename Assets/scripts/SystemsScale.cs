03f51dfc45577a0d65ece78e2ad9b4c915992484
timchoh585 <timchoh@gmail.com>
2017-10-22 00:23:45 -0500

    updated the menu

--- Assets/scripts/SystemsScale.cs
+++ Assets/scripts/SystemsScale.cs
@@ -19,10 +19,7 @@ public class SystemsScale : MonoBehaviour
     {
         if (isScaling)
         {
-            transform.localScale = new Vector3(originalSize * scale, transform.localScale.y, originalSize * scaleVariable);
-            ///transform.GetComponent<Circle>().xradius = originalSize * scaleVariable;
-           // transform.GetComponent<Circle>().yradius = originalSize * scaleVariable;
-            //Debug.Log("Scaliiing");
+            transform.localScale = new Vector3(originalSize * scale, transform.localScale.y, originalSize * scale);
         }
     }
 }
